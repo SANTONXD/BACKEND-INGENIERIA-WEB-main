3765359362aad4e9890a53f78e0a69d7b7b097f4
SANTONXD <santiago1210molina@gmail.com>
2025-10-28 16:59:21 -0500

    Add route for viewing messages

--- contacto/urls.py
+++ contacto/urls.py
@@ -3,4 +3,6 @@ from .views import MensajeAPIView
 
 urlpatterns = [
     path('mensaje/', MensajeAPIView.as_view(), name='api-mensaje'),
+    path('ver/', VerMensajesAPIView.as_view(), name='ver-mensajes'),
 ]
+
