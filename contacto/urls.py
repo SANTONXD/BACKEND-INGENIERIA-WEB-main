ae92ba54b6df4339ccb2e6921b70b54ef673b8ae
SANTONXD <santiago1210molina@gmail.com>
2025-10-28 13:13:20 -0500

    Change API endpoint from 'contacto' to 'mensaje'

--- contacto/urls.py
+++ contacto/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import MensajeAPIView
 
 urlpatterns = [
-    path('api/contacto/', MensajeAPIView.as_view(), name='api-contacto'),
+    path('mensaje/', MensajeAPIView.as_view(), name='api-mensaje'),
 ]
